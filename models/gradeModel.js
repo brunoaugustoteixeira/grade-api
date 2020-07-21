0b0df322b20a7c96585e3f6501090031d99025d4
bruno.teixeira <brunoaugustoteixeira@igti.com.br>
2020-07-20 23:49:37 -0300

    Sobrecarga toJSON

--- models/gradeModel.js
+++ models/gradeModel.js
@@ -7,6 +7,14 @@ export default (mongoose) => {
     lastModified: { type: Date, default: Date.now },
   });
 
+  schema.method('toJSON', function () {
+    const { __v, _id, ...object } = this.toObject();
+
+    object.id = _id;
+
+    return object;
+  });
+
   const Grade = mongoose.model('grade', schema, 'grade');
 
   return Grade;
